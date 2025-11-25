1b283acdad5f5466ca743594ec20b298a77e4194
I578070 <mohit.naskar@sap.com>
2025-11-25 20:46:22 +0530

    removed lazy loading

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -37,7 +37,7 @@ const Hero = () => {
       </div>
       
       <div className="absolute inset-0 top-20 flex justify-center items-center -z-10 pt-30">
-        <img src={avator} alt="avator" className="w-[750px] opacity-60 object-contain" loading="lazy"/>
+        <img src={avator} alt="avator" className="w-[750px] opacity-60 object-contain"/>
       </div>
       
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
