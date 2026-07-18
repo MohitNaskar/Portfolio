476fca5709111a4204118fb031ad392766f072ea
I578070 <mohit.naskar@sap.com>
2026-07-18 18:10:36 +0530

    bg primary changed to black

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#030319ff",
+        primary: "rgb(0, 0, 0)",
         secondary: "#aaa6c3",
         tertiary: "#151030",
         "black-100": "#100d25",
